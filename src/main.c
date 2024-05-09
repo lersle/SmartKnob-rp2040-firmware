69d503528012448947fa3ab2e9f9c1246fcd6477
Lars Erni <lars.erni@gmx.ch>
2024-05-09 22:12:32 +0200

    flashed blink on custom board

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@
 
 void led_task()
 {   
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    const uint LED_PIN = 29;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     while (true) {
         gpio_put(LED_PIN, 1);
-        vTaskDelay(100);
+        vTaskDelay(2000);
         gpio_put(LED_PIN, 0);
-        vTaskDelay(100);
+        vTaskDelay(2000);
     }
 }
 
